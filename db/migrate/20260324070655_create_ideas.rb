7675ebdc365ad0cd880444d230abd2750cb3cd6e
saekoy <saeconomail@gmail.com>
2026-03-24 16:07:11 +0900

    bin/rails g model Idea

---
+++ db/migrate/20260324070655_create_ideas.rb
@@ -0,0 +1,12 @@
+class CreateIdeas < ActiveRecord::Migration[8.1]
+  def change
+    create_table :ideas do |t|
+      t.string :title
+      t.text :description
+      t.string :category
+      t.integer :difficulty
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/ideas.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :idea do
+    title { "MyString" }
+    description { "MyText" }
+    category { "MyString" }
+    difficulty { 1 }
+  end
+end
